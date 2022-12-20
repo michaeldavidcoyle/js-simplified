727b408bfd977fe88c6e0535559bc0a5e38207b8
Michael Coyle <michael.david.coyle4@gmail.com>
2022-12-19 21:56:40 -0600

    Add event listener on form, prevent default, get input value and render to page.

--- js/advanced-todo.js
+++ js/advanced-todo.js
@@ -1,2 +1,30 @@
 'use strict';
-console.log('Hello from advanced-todo.js');
+
+const form = document.querySelector('#new-todo-form');
+const todoInput = document.querySelector('#todo-input');
+const list = document.querySelector('#list');
+const template = document.querySelector('#list-item-template');
+
+// Add Todos
+// User will type in todo and click add todo button.
+form.addEventListener('submit', event => {
+    event.preventDefault();
+
+    const todoName = todoInput.value;
+    if (todoName === '') return;
+
+    renderTodo(todoName);
+    todoInput.value = '';
+});
+
+function renderTodo(todoName) {
+    const templateClone = template.content.cloneNode(true);
+    // console.log(templateClone);
+    const textElement = templateClone.querySelector('[data-list-item-text]');
+    textElement.innerText = todoName;
+    list.appendChild(templateClone);
+}
+// This should then add the todo to the list above.
+// Delete Todos
+// Save Todos
+// Load Todos
