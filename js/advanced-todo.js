f79c48d1cfc7b380fbea73640c9c87a7c8738542
Michael Coyle <michael.david.coyle4@gmail.com>
2022-12-19 22:09:28 -0600

    Create function to save todos.

--- js/advanced-todo.js
+++ js/advanced-todo.js
@@ -4,6 +4,9 @@ const form = document.querySelector('#new-todo-form');
 const todoInput = document.querySelector('#todo-input');
 const list = document.querySelector('#list');
 const template = document.querySelector('#list-item-template');
+const todos = [];
+const LOCAL_STORAGE_PREFIX = 'ADVANCED_TODO_LIST';
+const TODOS_STORAGE_KEY = `${LOCAL_STORAGE_PREFIX}-todos`;
 
 // Add Todos
 // User will type in todo and click add todo button.
@@ -11,9 +14,12 @@ form.addEventListener('submit', event => {
     event.preventDefault();
 
     const todoName = todoInput.value;
+
     if (todoName === '') return;
 
+    todos.push(todoName);
     renderTodo(todoName);
+    saveTodos();
     todoInput.value = '';
 });
 
@@ -21,10 +27,14 @@ function renderTodo(todoName) {
     const templateClone = template.content.cloneNode(true);
     // console.log(templateClone);
     const textElement = templateClone.querySelector('[data-list-item-text]');
+// Add the todo to the list above.
     textElement.innerText = todoName;
     list.appendChild(templateClone);
 }
-// This should then add the todo to the list above.
-// Delete Todos
+
 // Save Todos
+function saveTodos() {
+    localStorage.setItem(TODOS_STORAGE_KEY, JSON.stringify(todos));
+}
+// Delete Todos
 // Load Todos
