176877a88f30af1662acafc66adbeee1e030b998
Michael Coyle <michael.david.coyle4@gmail.com>
2022-12-21 21:14:21 -0600

    Add event listener to update checked/completed todos

--- js/advanced-todo.js
+++ js/advanced-todo.js
@@ -9,6 +9,19 @@ const TODOS_STORAGE_KEY = `${LOCAL_STORAGE_PREFIX}-todos`;
 const todos = loadTodos();
 todos.forEach(renderTodo);
 
+list.addEventListener('change', event => {
+    if (!event.target.matches('[data-list-item-checkbox]')) return;
+
+    // Get the todo that is clicked
+    const parent = event.target.closest('.list-item');
+    const todoId = parent.dataset.todoId;
+    const todo = todos.find(t => t.id === todoId);
+    // toggle the complete property to be equal to the checkbox value
+    todo.complete = event.target.checked;
+    // Save our update todo
+    saveTodos();
+})
+
 // Add Todos
 // User will type in todo and click add todo button.
 form.addEventListener('submit', event => {
@@ -18,18 +31,26 @@ form.addEventListener('submit', event => {
 
     if (todoName === '') return;
 
-    todos.push(todoName);
-    renderTodo(todoName);
+    const todo = {
+        name: todoName,
+        complete: false,
+        id: Date.now().toString()
+    }
+
+    todos.push(todo);
+    renderTodo(todo);
     saveTodos();
     todoInput.value = '';
 });
 
-function renderTodo(todoName) {
+function renderTodo(todo) {
     const templateClone = template.content.cloneNode(true);
+    const listItem = templateClone.querySelector('.list-item');
+    listItem.dataset.todoId = todo.id;
     // console.log(templateClone);
     const textElement = templateClone.querySelector('[data-list-item-text]');
 // Add the todo to the list above.
-    textElement.innerText = todoName;
+    textElement.innerText = todo.name;
     list.appendChild(templateClone);
 }
 
