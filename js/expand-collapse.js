48d4e167160f56880e0aa1e566899039be2a2635
Michael Coyle <michael.david.coyle4@gmail.com>
2022-12-26 21:20:05 -0600

    add click listener for expand/collapse buttons

--- js/expand-collapse.js
+++ js/expand-collapse.js
@@ -1,2 +1,11 @@
 'use strict';
-console.log('hello from expand-collapse.js')
+
+// TODO: add click listeners to expand/collapse buttons
+document.addEventListener('click', event => {
+    if (!event.target.matches('.expand-button')) return;
+
+    console.log('expand/collapse clicked');
+});
+// TODO: add show class to cards when expand is clicked
+// TODO: toggle button innerText when clicked
+// TODO: remove show class when collapse is clicked
