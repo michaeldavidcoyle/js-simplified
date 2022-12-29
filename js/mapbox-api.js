faf6cfcba359090262bc7dc624be4624ec40e0ab
Michael Coyle <michael.david.coyle4@gmail.com>
2022-12-28 20:34:49 -0600

    add geolocation

--- js/mapbox-api.js
+++ js/mapbox-api.js
@@ -1,7 +1,25 @@
 'use strict';
 
-var map = new mapboxgl.Map({
-    accessToken: MAPBOX_ACCESS_TOKEN,
-    container: 'map',
-    style: 'mapbox://styles/mapbox/streets-v11'
-});
+navigator.geolocation.getCurrentPosition(
+    successLocation,
+    errorLocation,
+    { enableHighAccuracy: true }
+)
+
+function setupMap(centerPosition) {
+    const map = new mapboxgl.Map({
+        accessToken: MAPBOX_ACCESS_TOKEN,
+        container: 'map',
+        style: 'mapbox://styles/mapbox/streets-v11',
+        center: centerPosition,
+        zoom: 12
+    });
+}
+
+function successLocation(position) {
+    setupMap([position.coords.longitude, position.coords.latitude])
+}
+
+function errorLocation() {
+    setupMap([-87.6298, 41.8781])
+}
