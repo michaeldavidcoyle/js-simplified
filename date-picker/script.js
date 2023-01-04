7af5e67f87280ad0de2b9fc13fdf8c5a47d663e3
Michael Coyle <michael.david.coyle4@gmail.com>
2023-01-03 22:58:14 -0600

    add event listener to date button to hide/show modal

--- date-picker/script.js
+++ date-picker/script.js
@@ -3,3 +3,7 @@ import { add, format } from 'date-fns';
 const today = new Date();
 const dateBtn = document.querySelector('.date-picker-button');
 dateBtn.innerText = format(today, 'MMMM do yyyy');
+
+dateBtn.addEventListener('click', event => {
+    event.target.closest('.date-picker-container').querySelector('.date-picker').classList.toggle('show');
+})
