d0eaef004d006113c02b195beec1d1e9ac2d1887
Michael Coyle <michael.david.coyle4@gmail.com>
2023-01-04 23:56:36 -0600

    set current month name and dates

--- date-picker/script.js
+++ date-picker/script.js
@@ -1,9 +1,33 @@
-import { add, format } from 'date-fns';
+import { add, format, getDaysInMonth } from 'date-fns';
 
 const today = new Date();
 const dateBtn = document.querySelector('.date-picker-button');
 dateBtn.innerText = format(today, 'MMMM do yyyy');
 
+const currentMonthHeader = document.querySelector('.current-month');
+const dateButtons = document.querySelectorAll('.date');
+
 dateBtn.addEventListener('click', event => {
     event.target.closest('.date-picker-container').querySelector('.date-picker').classList.toggle('show');
-})
+    currentMonthHeader.innerText = format(today, 'MMMM - yyyy');
+    let month = new Month(today);
+    const weekStart = month.weekdayStart();
+    for (let d = weekStart; d < month.totalDays; d++) {
+        dateButtons[d].innerText = d + 1;
+        if (today.getDate() === d + 1) {
+            dateButtons[d].classList.add('selected');
+        } else {
+            dateButtons[d].classList.remove('selected');
+        }
+    }
+});
+
+function Month(selectedDate) {
+    this.year = selectedDate.getFullYear();
+    this.month = selectedDate.getMonth();
+    this.date = selectedDate.getDate();
+    this.weekdayStart = function() {
+        return new Date(this.year, this.month, 1).getDay();
+    }
+    this.totalDays = getDaysInMonth(selectedDate);
+}
