87eacc83d702c19d3903bcd9a6db5001b3d74c8c
Michael Coyle <michael.david.coyle4@gmail.com>
2023-01-05 16:33:37 -0600

    add event listener on datePickerGrid for date selection

--- date-picker/script.js
+++ date-picker/script.js
@@ -1,4 +1,4 @@
-import { add, format, getDaysInMonth } from 'date-fns';
+import {add, format, getDaysInMonth} from 'date-fns';
 
 const today = new Date();
 const datePickerBtn = document.querySelector('.date-picker-button');
@@ -6,29 +6,37 @@ const currentMonthHeader = document.querySelector('.current-month');
 const dateButtons = document.querySelectorAll('.date');
 const prevMonthBtn = document.querySelector('.prev-month-button');
 const nextMonthBtn = document.querySelector('.next-month-button');
+const datePickerGrid = document.querySelector('.date-picker-grid-dates');
+
+let currentMonth;
+let selectedDate = today;
 
 datePickerBtn.innerText = format(today, 'MMMM do yyyy');
 currentMonthHeader.innerText = format(today, 'MMMM - yyyy');
 
-let currentMonth = new Month(today);
-currentMonth.setCalendar();
+function makeCalendar(selectedDate) {
+    currentMonth = new Month(selectedDate);
+    currentMonth.setCalendar();
 
-for (let d = 0; d < dateButtons.length; d++) {
-    let currentDate = currentMonth.calendar[d];
-    dateButtons[d].innerText = currentDate.getDate();
+    for (let d = 0; d < dateButtons.length; d++) {
+        let currentDate = currentMonth.calendar[d];
+        dateButtons[d].innerText = currentDate.getDate();
 
-    if (currentDate.getMonth() === currentMonth.month) {
-        dateButtons[d].classList.remove('date-picker-other-month-date');
-    } else {
-        dateButtons[d].classList.add('date-picker-other-month-date');
-    }
+        if (currentDate.getMonth() === currentMonth.month) {
+            dateButtons[d].classList.remove('date-picker-other-month-date');
+        } else {
+            dateButtons[d].classList.add('date-picker-other-month-date');
+        }
 
-    if (today.getDate() === currentDate.getDate()) {
-        dateButtons[d].classList.add('selected');
-    } else {
-        dateButtons[d].classList.remove('selected');
+        if (today.getDate() === currentDate.getDate()) {
+            dateButtons[d].classList.add('selected');
+        } else {
+            dateButtons[d].classList.remove('selected');
+        }
     }
 }
+
+makeCalendar(today);
 
 datePickerBtn.addEventListener('click', event => {
     event.target
@@ -38,8 +46,19 @@ datePickerBtn.addEventListener('click', event => {
         .toggle('show');
 });
 
+datePickerGrid.addEventListener('click', event => {
+    if (!event.target.matches('.date')) return;
+
+    for (let d = 0; d < dateButtons.length; d++) {
+        dateButtons[d].classList.remove('selected');
+    }
+    event.target.classList.add('selected');
+
+    selectedDate = currentMonth.calendar[event.target.dataset.index];
+    datePickerBtn.innerText = format(selectedDate, 'MMMM do yyyy');
+});
+
 prevMonthBtn.addEventListener('click', event => {
-    console.log('prevMonthBtn clicked');
 });
 
 nextMonthBtn.addEventListener('click', event => {
@@ -52,16 +71,16 @@ function Month(selectedDate) {
     this.date = selectedDate;
     this.totalDays = getDaysInMonth(selectedDate);
     this.calendar = [];
-    this.prevMonth = add(selectedDate, { months: -1 });
-    this.nextMonth = add(selectedDate, { months: 1 });
-    this.weekdayStart = function() {
+    this.prevMonth = add(selectedDate, {months: -1});
+    this.nextMonth = add(selectedDate, {months: 1});
+    this.weekdayStart = function () {
         return new Date(this.year, this.month, 1).getDay();
     }
-    this.setCalendar = function() {
+    this.setCalendar = function () {
         let offset = -this.weekdayStart();
-        const startDate = add(this.date, { days: -this.date.getDate() - offset++ });
+        const startDate = add(this.date, {days: -this.date.getDate() - offset++});
         while (this.calendar.length < dateButtons.length) {
-            let date = add(startDate, { days: offset++ });
+            let date = add(startDate, {days: offset++});
             this.calendar.push(date);
         }
     }
